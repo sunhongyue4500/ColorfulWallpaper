6ee75dbb005914c6054fdb336c9f7f19443b7bf5
hongyi <hong6yi@gmail.com>
2017-08-28 22:59:38 +0800

    initial commit

---
+++ ColorWallpaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorWallpaper
+//
+//  Created by Sunhy on 2017/8/28.
+//  Copyright © 2017年 Sunhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorWallpaper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorWallpaper
+//
+//  Created by Sunhy on 2017/8/28.
+//  Copyright © 2017年 Sunhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
